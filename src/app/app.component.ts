8ae07627c1ba3d7f76dafaa939ffb64280cce677
JrealFun <jeraldlee2002@gmail.com>
2024-08-24 13:07:56 +0800

    feature: route navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SpecialComponent } from './special/special.component';
 import { NormalComponent } from './normal/normal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NormalComponent, SpecialComponent],
+  imports: [RouterOutlet, NormalComponent, RouterLink, SpecialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NormalComponent } from './normal/normal.component';
+import { AnotherComponent } from './another/another.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'normal-component', component: NormalComponent },
+  { path: 'another-component', component: AnotherComponent },
+  { path: '**', component: AppComponent },
+];
